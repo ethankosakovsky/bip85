7f7e6882457d54b387219c72b2fc38fb65e312ff
Ethan Kosakovsky <ethankosakovsky@protonmail.com>
2020-10-17 13:12:53 +0000

    clarify vars

--- bip85/app.py
+++ bip85/app.py
@@ -36,29 +36,29 @@ def bip39(xprv_string, language, words, index):
         'czech': 8
     }
     lang_code = language_lookup[language]
-    e = BIP85()
+    bip85 = BIP85()
     path = f"83696968p/39p/{lang_code}p/{words}p/{index}p"
 
-    entropy = e.bip32_xprv_to_entropy(path, xprv_string)
-    return e.entropy_to_bip39(entropy, words, language)
+    entropy = bip85.bip32_xprv_to_entropy(path, xprv_string)
+    return bip85.entropy_to_bip39(entropy, words, language)
 
 
 def wif(xprv_string, index):
     # m/83696968'/2'/index'
-    e = BIP85()
+    bip85 = BIP85()
     path = f"83696968p/2p/{index}p"
-    return e.entropy_to_wif(e.bip32_xprv_to_entropy(path, xprv_string))
+    return bip85.entropy_to_wif(bip85.bip32_xprv_to_entropy(path, xprv_string))
 
 
 def hex(xprv_string, index, width):
     # m/83696968'/128169p'/index'
-    e = BIP85()
+    bip85 = BIP85()
     path = f"83696968p/128169p/{width}p/{index}p"
-    return e.bip32_xprv_to_hex(path, width, xprv_string)
+    return bip85.bip32_xprv_to_hex(path, width, xprv_string)
 
 
 def xprv(xprv_string, index):
     # 83696968'/32'/index'
-    e = BIP85()
+    bip85 = BIP85()
     path = f"83696968p/32p/{index}p"
-    return e.bip32_xprv_to_xprv(path, xprv_string)
+    return bip85.bip32_xprv_to_xprv(path, xprv_string)
