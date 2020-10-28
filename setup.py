6c53abb289246e7f6dcd68eba7fc238bfa83d285
Ethan Kosakovsky <ethankosakovsky@protonmail.com>
2020-10-28 22:25:14 +0000

    Update package to v0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "rt") as fh:
 
 setuptools.setup(
     name="bip85",
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
     author="Ethan Kosakovsky",
     author_email="ethankosakovsky@protonmail.com",
@@ -16,11 +16,14 @@ setuptools.setup(
     url="https://github.com/ethankosakovsky/bip85",
     packages=setuptools.find_packages(),
     keywords=['BIP85', 'entropy', 'bitcoin'],
-    install_requires=["mnemonic", "pycoin", "base58", "pytest", "monero"],
+    install_requires=["mnemonic", "pycoin", "base58", "monero", "pycryptodome"],
+    tests_require=["pytest"],
+    python_requires=">=3.6",
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Security :: Cryptography",
     ],
 )
